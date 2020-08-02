5490c2fd7dfa2e847b6213ad4cd2aa0abe3b8ccf
sam landman <sam landman>
2020-08-02 11:55:09 +0100

    inserted logic for creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  private
+  def  user_params
+    params.require(:user).permit(:username,:password)
   end
 end
