1ffe618798d13237af8ebe7c90afcf56537aec67
sam landman <sam landman>
2020-08-02 11:48:17 +0100

    Created index page with div ids for each post based on index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post =   Post.find(params[:id])
+  end
+
   def new
   end
+
+  def create
+    p params
+    @post = Post.new(post_params)
+    p post_params
+    @post.save
+    redirect_to @post
+  end
+
+ 
+
+
+  private
+  def post_params
+    params.require(:posts).permit(:title, :text, :image)
+  end
+
 end
